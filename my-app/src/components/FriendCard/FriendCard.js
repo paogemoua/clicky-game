6121a6b4cf40f4e4d07ffe27f9c001e779d5e310
Paoge Moua <paoge.moua@gmail.com>
2019-02-16 06:54:20 -0600

    Updates to Friend-js

--- my-app/src/components/FriendCard/FriendCard.js
+++ my-app/src/components/FriendCard/FriendCard.js
@@ -4,7 +4,7 @@ import "./FriendCard.css";
 var FriendCard = props => (
     <div FriendCard="card" onClick={props.clickedBurgers}>
         <div className="img-container">
-            <img alt={props.image.replace(".jpg", "")} src={requestAnimationFrame("../../images/" + props.image)} />
+            <img alt={props.image.replace(".jpg", "")} src={requestAnimationFrame("./clicky-game/my-app/src/images" + props.image)} />
         </div>
     </div>
 );
